30461fcb24b126e9e3df2bf853c3b789cfb07c55
adapiedrafita <ada.piedrafita.esteban@emeal.nttdata.com>
2023-05-12 11:39:56 +0200

    Models definition

--- Models/GameItem.cs
+++ Models/GameItem.cs
@@ -2,10 +2,10 @@ namespace videogames.Models;
 
     public class Game
 {
-    public string Name { get; set;}
+    public string Name { get; set; }
     public string Description { get; set; }
     public int Quantity{ get; set; }
-    public int Price{get;set;}
+    public int Price{ get; set; }
     public int Id{ get; set; }
 
     public Game(){}
